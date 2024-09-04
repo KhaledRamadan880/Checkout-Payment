70f06d23885df284b96053c90c0eb70b1b156388
KhaledRamadan880 <khaledramdan880@gmail.com>
2024-09-04 14:33:10 +0200

    Create Project

---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CheckoutPaymentApp extends StatelessWidget {
+  const CheckoutPaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:checkout_payment/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckoutPaymentApp());
+}
